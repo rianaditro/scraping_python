787df91fa4d534df408aabe0081e5b4e092b21ef
rianaditro <rianaditro@gmail.com>
2023-11-03 00:56:02 +0700

    bug : inconcistency tag, restart

--- scrap5.py
+++ scrap5.py
@@ -1,12 +1,10 @@
 from requests_html import HTMLSession
 from bs4 import BeautifulSoup
 import pandas as pd
-import time
 
 def access_url(url):
     session = HTMLSession()
     r = session.get(url) #response [200]
-    print(r)
     r.html.render()
     text = r.html.html
     return text
@@ -18,13 +16,12 @@ def write_html_file(text):
 def read_html_file(file):
     with open(f"{file}","r" ,encoding="UTF-8") as f:
         text = f.read()
-    text = BeautifulSoup(text, "html.parser")
     return text
 
-
-def html_parse(html_file): #fungsinya buat apa, for parsing, the output or return value, should be a dictionary. ex = {"title":ÖSTANÖ} dst
+def html_parse(html_file):
+    html_file = BeautifulSoup(html_file,"html.parser")
     title = html_file.find("div", class_="d-flex flex-row").get_text().strip()
-    price = html_file.find("p", class_="itemBTI display-6").get_text().replace("Rp", "").replace(".", "").strip()
+    price = html_file.find("div", class_="itemPrice-wrapper").get_text().replace("Rp", "").replace(".", "").strip()
     desc = html_file.find("span", class_="itemFacts font-weight-normal").get_text().replace("\n","")
     item_code = html_file.find("span",class_="item-code").get_text().strip()
     designer = html_file.find("div", id="good-to-know").find("div").find_all("div")[-1].get_text().strip()
@@ -32,13 +29,13 @@ def html_parse(html_file): #fungsinya buat apa, for parsing, the output or retur
     length = measure[5].get_text().strip().replace(" cm", "")
     width = measure[7].get_text().strip().replace(" cm", "")
     height = measure[9].get_text().strip().replace(" cm", "")
-    details = html_file.find("div", class_="product-desc-wrapper mb-4").p.get_text().strip()
-    image_list = []
-    image = html_file.find_all("div", class_="image-container slick-slide")
-    for div in image:
-        img = div.find("span").find("img")["data-lazy"]
-        image_list.append(img)
+    img = html_file.find("img", class_="img-fluid img-nr-0")["src"]
 
+    try:
+        details = html_file.find("div", class_="product-desc-wrapper mb-4").p.get_text().strip()
+    except AttributeError:
+        details = ""
+    
     dict_result = {"Nama Produk":title,
                    "Harga":price,
                    "Deskripsi":desc,
@@ -48,13 +45,13 @@ def html_parse(html_file): #fungsinya buat apa, for parsing, the output or retur
                     "Tinggi":height,
                     "Desainer":designer,
                     "Rincian Produk":details,
-                    "Gambar":image_list[0]
+                    "Gambar":img
                     }
     return dict_result
 
 
 if __name__ == "__main__": 
-    url = "https://www.ikea.co.id/in/produk/kursi-makan/kursi-berpelapis/"
+    url = "https://www.ikea.co.id/in/produk/peralatan-dapur/perkakas-dapur/idealisk-art-90165968"
 
     access = access_url(url)
 
@@ -62,8 +59,9 @@ if __name__ == "__main__":
     
     html_file = read_html_file("output.html")
     
-    item_url = html_file.find_all("div", class_="d-flex flex-row")
-    print(item_url)
+    result = html_parse(html_file)
+
+    print(result)
     
 
 """
